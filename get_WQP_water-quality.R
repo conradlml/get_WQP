6d126c8b7055a1ae72c141b4130dcfc7526c27b6
Lily Conrad <193945173+conradlml@users.noreply.github.com>
2025-02-05 12:33:49 -0800

    Add files via upload

---
+++ get_WQP_water-quality.R
@@ -0,0 +1,89 @@
+
+################################################################################
+# Direct water quality data download from the Water Quality Portal. 
+# 
+# Lily Conrad, IDEQ State Office
+# last update: 1/3/2025
+#
+# Data citation: 
+# Water Quality Portal. Washington (DC): National Water Quality Monitoring Council, 
+# United States Geological Survey (USGS), Environmental Protection Agency (EPA); 
+# 2021. https://doi.org/10.5066/P9QRKUVJ.
+################################################################################
+
+# To get started, provide the user inputs below following the outlined format.
+
+
+### User inputs ----------------------------------------------------------------
+
+# Enter the eight digit HUC that you'd like to download data for. If you want to
+# look at multiple HUCs, separate them by a comma inside the parentheses. 
+huc <- c(17010305, 170010304)
+
+# Enter the start date (mm-dd-yyyy) for your period of interest.
+start <- "01-01-2015"
+
+# Enter the end date (mm-dd-yyyy) for your period of interest.
+end <- "12-31-2024"
+
+# Enter your parameter(s) of interest. If you would like to list multiple, 
+# follow the format outlined below. If you aren't sure what exact word to type,
+# Check the Characteristic list available for selection on the WQP website. 
+characteristicName <- c("Temperature", "Phosphorus", "Copper", "Microcystin")
+
+# Enter your username (the name at the beginning of your computer's file explorer
+# path) in quotations.
+my_name <- "jdoe"
+
+
+# Now that you've entered the values above, click on "Source" and watch
+# your console for errors. If the script ran successfully, there will be a new
+# Excel file in your Downloads folder. 
+
+
+################################################################################
+#                                 START
+################################################################################
+
+### Load packages and data -----------------------------------------------------
+
+my_packages <- c("dplyr", "openxlsx", "dataRetrieval")
+install.packages(my_packages, repos = "http://cran.rstudio.com")
+
+library(dplyr)
+library(openxlsx)
+library(dataRetrieval)
+
+
+### Retrieve water quality data ------------------------------------------------
+
+# Data query.            
+rawdat <- readWQPdata(huc = huc,
+                      startDate = start,
+                      endDate = end,
+                      characteristicName = characteristicName)
+
+# Site query.
+sites <- whatWQPsites(huc = huc,
+                      startDate = start,
+                      endDate = end,
+                      characteristicName = characteristicName,
+                      ActivityMedianName = "Water",
+                      ActivityMediaSubdivisionName = "Surface Water")
+
+# Add in site names.
+wq.dat <- merge(x = rawdat, y = sites, by = "MonitoringLocationIdentifier")
+
+# Remove QC and non-surface water samples
+wq.dat <- wq.dat %>%
+  filter(ActivityMediaSubdivisionName == "Surface Water" & 
+           ActivityTypeCode == "Sample-Routine")
+
+# Save the data to excel. This will save the file in your downloads folder
+# Adjust the file path if you'd like it to save somewhere else.
+write.xlsx(wq.dat, paste0("C:/Users/", my_name,"/Downloads/",Sys.Date(),"WQP_download.xlsx"))
+
+
+################################################################################
+#                                 END
+################################################################################
